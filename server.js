8223ac300acd02e649454d6a836421a7b14c6763
lrf141 <ghost141.kentyo@gmail.com>
2018-12-09 16:53:36 +0900

    redis周りのコードを追加

--- server.js
+++ server.js
@@ -1,4 +1,91 @@
-const http = require('http');
+const Http = require('http');
+const Redis = require('ioredis');
+const Url = require('url');
+const Query = require('querystring');
+const Fs = require('fs');
+const Tmp = require('tmp');
+const Colors = require('colors/safe');
 
-const server = http.createServer(function (request, response) {  
+
+// redis config
+const redisConfig = require('./redis-config.json');
+const queueConfig = require('./redis-queue-config.json');
+const redis = new Redis(redisConfig);
+const sub = new Redis(redisConfig);
+const pub = new Redis(redisConfig);
+
+// start redis subscribe channel
+sub.subscribe('waiting-queue-event', 'processing-queue-event', (err, count) => {
+	// no event
+    if(err)
+        console.log(err);
 });
+
+// server config
+const proxyPort = process.env.ACCEPTED_PORT || 9000;
+
+
+// http condfig
+const defaultHeader = require('./default-http-header.json');
+
+
+const getFromClient = (request, response) => {
+
+	let urlParts = Url.parse(request.url, true);
+	switch(urlParts.pathname){
+    
+	case '':
+	case '/':
+		response.writeHead(200, defaultHeader);
+		response.end(JSON.stringify({'status': 'success'}));
+		break;
+
+	case '/add':
+		if (request.method === 'POST') {
+                
+			let data = '';
+                
+			request.on('data', (chunk) => {
+				data += chunk;
+			});
+
+			request.on('end', () => {
+				const jsonData = JSON.parse(data);
+				if (jsonData.xid && jsonData.code) {
+					Fs.writeFileSync('./examples/' + jsonData.xid + '.py', jsonData.code);
+                        
+					//add error handler
+					pub.pipeline().publish('waiting-queue-event', JSON.stringify({'event': 'add', 'id': jsonData.xid, 'code': jsonData.code})).exec();
+					redis.pipeline().rpush('waitQueue', jsonData.xid+'.py').exec();
+                        
+					response.writeHead(200, defaultHeader);
+					response.end(JSON.stringify({'status': 'success'}));
+				} else {
+					response.writeHead(400, defaultHeader);
+					response.end(JSON.stringify({'status': 'bad request.'}));
+				}
+			});
+		} else {
+			response.writeHead(405, defaultHeader);
+			response.end(JSON.stringify({'status': 'Method Not Allowed..'}));
+		}
+		break;
+
+	default:
+		response.writeHead(404, defaultHeader);
+		response.end(JSON.stringify({'status': 'not found'}));
+		break;
+	}
+};
+
+// launch proxy server
+const server = Http.createServer(getFromClient);
+server.listen(proxyPort);
+
+
+// print server status
+console.log('Accepted-Proxy running on ' + proxyPort + ' with node: ' + process.version + ' pid: ' + process.pid + ' ... ' + Colors.green('[OK]'));
+console.log('Redis connect ... ' + Colors.green('[OK]'));
+console.log('Redis subscribe connect ... ' + Colors.green('[OK]'));
+console.log('Redis publish connect ... ' + Colors.green('[OK]'));
+console.log('To shutdown, press <CTRL>+C at any time.');
